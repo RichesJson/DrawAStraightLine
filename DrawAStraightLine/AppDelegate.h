4a821d4ac054ad4ca2e41c5f6b1d4557426285f0
jecons@live.cn <jecons@live.cn>
2017-10-20 21:24:06 +0800

    Initial Commit

---
+++ DrawAStraightLine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawAStraightLine
+//
+//  Created by richsjeson on 2017/10/20.
+//  Copyright © 2017年 richsjeson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
